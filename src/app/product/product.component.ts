a9dd1e5325e99eac5ad9fcd12a02c821e8a98704
enesturan00@gmail.com <enesturan00@gmail.com>
2019-10-24 15:10:26 +0300

    hede

---
+++ src/app/product/product.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from './product';
+import { AlertifyService } from '../services/alertify.service'
+import { HttpClient } from '@angular/common/http'
+
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent implements OnInit {
+
+  constructor(private alertifyService: AlertifyService, private http: HttpClient) { }
+  title = "Ürün Listesi"
+  filterText = ""
+  products: Product[];
+  path = "http://localhost:3000/products"
+
+
+  ngOnInit() {
+    this.http
+      .get<Product[]>(this.path)
+      .subscribe(data => {
+        this.products = data
+      });
+  }
+
+  addToCard(product) {
+    this.alertifyService.success(product.name + "added")
+  }
+
+
+}
